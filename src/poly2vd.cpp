2aaaa773bc0af548c512272c890caf677f1cea82
Tomas Juchelka <tomas.juchelka@gmail.com>
2012-01-07 13:16:00 +0100

    changed input method for polygons

--- src/poly2vd.cpp
+++ src/poly2vd.cpp
@@ -28,17 +28,19 @@
 #include "dvi_graphics_header.h"	// beacause of API_InitializeProgram()
 #include "ext_appl_inout.h"
 
-boolean poly2VD(void)
+boolean poly2VD(in_segs * segs, unsigned int size)
 {
-	boolean input_ok = false;
-	std::cout << "start" << std::endl;
+//	boolean input_ok = false;
+	boolean new_input = true;
 	API_InitializeProgram();
+	/*
 	char fileName[]="./util/fiddling_with_vroni/data/map_from_the_paper.dxf";
 	API_FileInput(fileName,&input_ok);
 	if (!input_ok) {
 		printf("Could not load data file\n");
 		return EXIT_FAILURE;
-	}
+	}*/
+	API_ArrayInput(0,NULL, size, segs,0,NULL, &new_input);
 
 	char ofile[]="";
 	boolean _false = false;
@@ -96,10 +98,59 @@ boolean poly2VD(void)
 //
 	API_TerminateProgram();
 	std::cout << "Program finished succesfully." << std::endl;
+	std::cout << "Output written to: " << o_file << std::endl;
 	return EXIT_SUCCESS;
 }				/* ----------  end of function poly2VD -------- */
 
+void convertPoly2segs(ClipperLib::Polygons & poly, in_segs * s, unsigned int size)
+{
+	in_segs init;
+	init.x1 = 0.0;
+	init.x2 = 0.0;
+	init.y1 = 0.0;
+	init.y2 = 0.0;
+	for (unsigned int i = 0; i < size; i++)
+		s[i] = init;
+	unsigned int k = 0;
+	for(unsigned int n = 0; n < poly.size(); n++)	{
+		unsigned int sz = poly[n].size();
+		for(unsigned int i = 0; i < sz - 1; i++)	{
+			s[k].x1 = (double)poly[n][i].X;
+			s[k].y1 = (double)poly[n][i].Y;
+			s[k].x2 = (double)poly[n][i+1].X;
+			s[k].y2 = (double)poly[n][i+1].Y;
+			k++;
+		}
+			s[k].x1 = (double)poly[n][sz-1].X;
+			s[k].y1 = (double)poly[n][sz-1].Y;
+			s[k].x2 = (double)poly[n][0].X;
+			s[k].y2 = (double)poly[n][0].Y;
+			k++;
+	}	
+}
+
 int main ( int argc, char *argv[] )
 {
-	return poly2VD();
+	ClipperLib::Polygons poly(1);
+	poly[0].push_back(ClipperLib::IntPoint(180,200));
+	poly[0].push_back(ClipperLib::IntPoint(260,200));
+	poly[0].push_back(ClipperLib::IntPoint(260,150));
+	poly[0].push_back(ClipperLib::IntPoint(180,150));
+	
+	unsigned int size = 0;
+	for(unsigned int i = 0; i < poly.size(); i++)
+		size += poly[i].size();
+
+	in_segs segs[size];
+	convertPoly2segs(poly, segs, size);
+	std::cout << "Polygon structure after conversion: " << std::endl;
+	for(unsigned int i = 0; i < size; i++) {
+		std::cout << "x1: " << segs[i].x1 << std::endl; 
+		std::cout << "y1: " << segs[i].y1 << std::endl; 
+		std::cout << "x2: " << segs[i].x2 << std::endl; 
+		std::cout << "y2: " << segs[i].y2 << std::endl; 
+		std::cout << "--------------" << std::endl; 
+	}
+	return poly2VD(segs, size);
+	//return 0;
 }				/* ----------  end of function main  ---------- */
