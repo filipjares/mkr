d6a550f7ff6403fd626eac2c68a14e1f6b93e7d0
Filip Jareš <filipjares@post.cz>
2012-04-23 10:52:18 +0200

    fix, amend commit ecc50ff107479a7088c68155c2db32f581f377d1
    add forgotten new VroniUtils.cpp

---
+++ src/VroniUtils.cpp
@@ -0,0 +1,26 @@
+/*
+ * ===========================================================================
+ *
+ *       Filename:  VroniUtils.cpp
+ *
+ *    Description:  Class containing static utility functions helping to cope
+ *                  with Vroni's internal data
+ *
+ *        Version:  1.0
+ *        Created:  04/20/2012 09:51:11 PM
+ *       Compiler:  gcc
+ *
+ *         Author:  Filip Jares (fj), filipjares@post.cz
+ *                  Tomas Juchelka (tj), tomas.juchelka@gmail.com
+ *
+ * ===========================================================================
+ */
+#include "VroniUtils.hpp"
+
+namespace poly2vd {
+
+	const std::string VroniUtils::site_type_names[] = {"SEG", "ARC", "PNT", "VDN", "VDE", "DTE", "CCW", "CW", "UNKNOWN" };
+
+}
+
+// vi:ai:sw=4 ts=4 sts=0 tw=120
